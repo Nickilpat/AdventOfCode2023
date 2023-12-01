d0c081550d52b072b70c99d3e7c8d51ab977c97a
Nick Kilpatrick <nkilpat1@ford.com>
2023-12-01 13:59:26 -0500

    nested the list comprehension to solve in 1 line

--- 01/sol.py
+++ 01/sol.py
@@ -4,8 +4,6 @@ input_file = open("01/input.txt", "r")
 calibration_lines = input_file.read().split('\n')
 input_file.close()
 
-calibration_long_numbers = [re.sub("\D","",x) for x in calibration_lines]
-calibration_numbers = [int(x[0] + (x[-1])) for x in calibration_long_numbers if x]
-calibration_sum = sum(calibration_numbers)
+calibration_sum = sum([int(x[0] + (x[-1])) for x in [re.sub("\D","",x) for x in calibration_lines] if x])
 
 print(calibration_sum)
